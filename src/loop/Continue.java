83adb4dcacfed9e4287fe013489c360dfdb8e979
Daeun Kim <dana1939@naver.com>
2024-11-19 11:07:27 +0900

    반복문1

---
+++ src/loop/Continue.java
@@ -0,0 +1,18 @@
+package loop;
+
+import java.awt.*;
+
+public class Continue {
+    public static void main(String[] args) {
+        int i = 1;
+
+        while ( i<= 5){
+            if (i == 3){
+                i++;
+                continue;
+            }
+            System.out.println(i);
+            i++;
+        }
+    }
+}
