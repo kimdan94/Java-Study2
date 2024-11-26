c8dd609db5eadc9b0b3da947118e5ba817f0581d
Daeun Kim <dana1939@naver.com>
2024-11-26 11:15:17 +0900

    배열 문제 풀이

---
+++ src/array/ArrayEx6.java
@@ -0,0 +1,32 @@
+package array;
+
+import java.util.Scanner;
+
+public class ArrayEx6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("입력받을 숫자의 개수를 입력하세요:");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        int minNum, maxNum;
+
+        System.out.println(n + "개의 정수를 입력하세요:");
+        for(int i=0;i<n;i++){
+            numbers[i] = scanner.nextInt();
+        }
+
+        minNum = maxNum = numbers[0];
+        for(int i=1;i<n;i++){
+            if(numbers[i]<minNum){
+                minNum = numbers[i];
+            }
+            if(numbers[i]>maxNum){
+                maxNum = numbers[i];
+            }
+        }
+
+        System.out.println("가장 작은 정수: " + minNum);
+        System.out.println("가장 큰 정수: " + maxNum);
+    }
+}
